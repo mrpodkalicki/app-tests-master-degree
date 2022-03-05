4d9fe2d76234476f66714d37d959f0738c16c702
Adrian Podkalicki <mr.podkalicki.adrian94@gmail.com>
2022-03-05 13:35:44 +0100

    adding style for home in angular app

--- angular-app/src/app/modules/home/home.component.ts
+++ angular-app/src/app/modules/home/home.component.ts
@@ -3,27 +3,34 @@ import {UiUrlUtils} from "../shared/utils/ui-url-utils";
 
 @Component({
   selector: 'app-home',
-  template: `
+  template:`
     <p>Initial tests</p>
-    <div>
+    <div class="btn-wrapper">
       <button class="btn" [routerLink]="['/'+ UI_URL.TEN_THOUSAND]">Go to 10 000 </button>
       <button  class="btn" [routerLink]="['/'+ UI_URL.ONE_HUNDRED_THOUSAND]">Go to 100 000 </button>
       <button  class="btn" [routerLink]="['/'+ UI_URL.FIVE_HUNDRED_THOUSAND]">Go to 500 000 </button>
     </div>
     <p>lazy-loading</p>
-    <div>
+    <div class="btn-wrapper">
       <button class="btn" [routerLink]="['/' + UI_URL.LAZY_LOADING_TEST + '/' + UI_URL.TEN_THOUSAND]">Go to 10 000 </button>
       <button  class="btn" [routerLink]="['/' + UI_URL.LAZY_LOADING_TEST + '/'+ UI_URL.ONE_HUNDRED_THOUSAND]">Go to 100 000 </button>
       <button  class="btn" [routerLink]="['/' + UI_URL.LAZY_LOADING_TEST + '/'+ UI_URL.FIVE_HUNDRED_THOUSAND]">Go to 500 000 </button>
     </div>
     <p>Create / update</p>
-    <div>
+    <div class="btn-wrapper">
       <button [routerLink]="['/'+ UI_URL.CREATE_DELETE_UPDATE_TEST]">Create/ updates tests </button>
     </div>
   `,
   styles: [`
-    .btn {
-        margin: 3px;
+    .btn-wrapper {
+      display: flex;
+      flex-direction: column;
+    }
+
+    .btn-wrapper > * {
+      margin: 5px;
+      width: 15rem;
+      height: 2rem;
     }
   `]
 })
